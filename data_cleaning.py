b8366ea305a200ff80b5079f2e56e34d53a594c7
Iman <kamkar.iman@gmail.com>
2021-01-20 20:31:31 +0330

    data cleaning script is modified

--- data_cleaning.py
+++ data_cleaning.py
@@ -6,6 +6,7 @@ Created on Sun Dec 20 14:09:17 2020
 @author: iman
 """
 import pandas as pd
+import re 
 data = pd.read_csv('glassdoor.csv') 
 df = data.copy()
 
@@ -23,9 +24,17 @@ df.columns = df.columns.str.lower()
 to_drop = df[(df['job description'] == '-1') | (df['salary estimate'] == '-1')].index
 df.drop(to_drop, axis=0, inplace=True)
 
+df['hourly'] = df['salary estimate'].apply(lambda x: 1 if 'per hour' in x.lower() else 0)
+
+df['employer_provided'] = df['salary estimate'].apply(lambda x: 1 if 'employer provided salary:' in x.lower() else 0)
+
+
+
+
 # remove the $ and 'K' signs and convert string values to integer
-salary = df['salary estimate'].apply(lambda x: x.replace('$', '')).\
-    apply(lambda x: x.replace('K', '')).str.split('-', expand=True).astype(int)
+salary = df['salary estimate'].apply(lambda x: re.findall('\$*[0-9]*K*-\$*[0-9]*K*', x)[0]).\
+    apply(lambda x: x.replace('$', '')).apply(lambda x: x.replace('K', '')).str.split('-', expand=True).astype(int)
+
 
 df['min_salary'] = salary[0]
 df['max_salary'] = salary[1]
@@ -55,34 +64,31 @@ df['job_state'] = df['job_state'].apply(lambda x: x if len(x)==2 else replace_st
 df['age'] = df['founded'].apply(lambda x: x if x<1 else 2020-x)
 
 ######## PARSING JOB DESCRIPTION
-## Python
-df['job description'] = df['job description'].apply(lambda x: x.lower() if type(x) is str else 'empty')
-df['python'] = df['job description'].apply(lambda x: 1 if 'python' in x else 0)
-print(df.python.value_counts())
+#parsing of job description (python, etc)
+#python
+df['python_yn'] = df['job description'].apply(lambda x: 1 if 'python' in x.lower() else 0)
+print(df.python_yn.value_counts())
+ 
+#r studio 
+df['R_yn'] = df['job description'].apply(lambda x: 1 if 'r studio' in x.lower() or 'r-studio' in x.lower() else 0)
+print(df.R_yn.value_counts())
 
-#### R
-df['R'] = df['job description'].apply(lambda x: 1 if 'r studio' in x.lower() or  
-                                      'r-studio' in x.lower() else 0)
-print(df.R.value_counts())
-
-#### Spark
+#spark 
 df['spark'] = df['job description'].apply(lambda x: 1 if 'spark' in x.lower() else 0)
-print(df.spark.value_counts())
+df.spark.value_counts()
 
-#### AWS
+#aws 
 df['aws'] = df['job description'].apply(lambda x: 1 if 'aws' in x.lower() else 0)
-print(df.aws.value_counts())
+df.aws.value_counts()
 
-###Cloud
-df['cloud'] = df['job description'].apply(lambda x: 1 if 'cloud' in x.lower() else 0)
-print(df.cloud.value_counts())
-
-### Excel
-df['excel'] = df['job description'].apply(lambda x: 1 if 'exel' in x.lower() else 0)
-print(df.excel.value_counts())
+#excel
+df['excel'] = df['job description'].apply(lambda x: 1 if 'excel' in x.lower() else 0)
+df.excel.value_counts()
 
 
-df.to_csv('salary-data-clean.csv', index=False)
+df_out = df.drop(['unnamed: 0'], axis =1)
+
+df_out.to_csv('salary_data_cleaned.csv',index = False)
 
 
 
